b01ae63bd8b797955dbd841a401e90569b32deba
Thales Santos <thalis.mraz@gmail.com>
2023-06-10 12:48:16 -0300

    :sparkles: feat: CRUD emprestimo pronto

--- src/api/controllers/emprestimoController.js
+++ src/api/controllers/emprestimoController.js
@@ -1,27 +1,77 @@
 const Emprestimo = require("../../database/models/emprestimoModel");
-const { Op } = require('sequelize'); // Importe o operador de consulta do Sequelize
 
-const emprestimoController = {};
-
-emprestimoController.listEmprestimo = async (req, res) => {
+const listEmprestimo = async (req, res) => {
   try {
-    const emprestimo = await Emprestimo.findAll();
-    res.status(200).json( emprestimo );
+    const emprestimos = await Emprestimo.findAll();
+    res.status(200).json(emprestimos);
   } catch (error) {
     res.status(500).json({ error });
   }
 };
 
-emprestimoController.createEmprestimo = async (req, res) => {
+const getEmprestimo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const emprestimo = await Emprestimo.findByPk(id);
+    if (!emprestimo) {
+      return res.status(404).json({ error: "Empréstimo não encontrado" });
+    }
+    res.status(200).json(emprestimo);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+const updateEmprestimo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { texto, nota } = req.body;
+
+    const emprestimo = await Emprestimo.findByPk(id);
+    if (!emprestimo) {
+      return res.status(404).json({ error: "Empréstimo não encontrado" });
+    }
+
+    emprestimo.texto = texto;
+    emprestimo.nota = nota;
+    await emprestimo.save();
+
+    res.status(200).json(emprestimo);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao atualizar o empréstimo" });
+  }
+};
+
+const deleteEmprestimo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const emprestimo = await Emprestimo.findByPk(id);
+    if (!emprestimo) {
+      return res.status(404).json({ error: "Empréstimo não encontrado" });
+    }
+
+    await emprestimo.destroy();
+
+    res.status(200).json({ message: "Empréstimo excluído com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao excluir o empréstimo" });
+  }
+};
+
+const createEmprestimo = async (req, res) => {
   try {
     const { texto, nota } = req.body;
     const emprestimo = await Emprestimo.create({ texto, nota });
     res.status(201).json(emprestimo);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao criar o usuário" });
+    res.status(500).json({ error: "Erro ao criar o empréstimo" });
   }
 };
 
-
-
-module.exports = emprestimoController;
+module.exports = {
+  listEmprestimo,
+  getEmprestimo,
+  updateEmprestimo,
+  deleteEmprestimo,
+  createEmprestimo,
+};
--- src/api/routes/emprestimoRoute.js
+++ src/api/routes/emprestimoRoute.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const emprestimo = require("../controllers/emprestimoController");
+const emprestimoController = require("../controllers/emprestimoController");
 
-router.get("/", emprestimo.listEmprestimo);
-router.post("/", emprestimo.createEmprestimo);
-// router.get("/:id", controller.getUsuario);
-// router.put("/:id", controller.updateUsuario);
-// router.delete("/:id", controller.deleteUsuario);
+router.get("/", emprestimoController.listEmprestimo);
+router.get("/:id", emprestimoController.getEmprestimo);
+router.post("/", emprestimoController.createEmprestimo);
+router.put("/:id", emprestimoController.updateEmprestimo);
+router.delete("/:id", emprestimoController.deleteEmprestimo);
 
 module.exports = router;
 
+
+
+// [
+//   {
+//     id: 1,
+//     data: "2001-01-20T23:00:00.000Z",
+//     dataDevolucao: "2010-02-20T23:00:00.000Z",
+//     createdAt: "2001-01-20T23:00:00.000Z",
+//     updatedAt: "2001-01-20T23:00:00.000Z",
+//   },
+// ];
+
+// {
+//   "data": "2000-01-20T23:00:00.000Z",
+//   "dataDevolucao": "2010-02-20T23:00:00.000Z"
+// }
+
+// {
+//   "data": "2023-06-10",
+//   "dataDevolucao": "2023-06-10"
+// }
